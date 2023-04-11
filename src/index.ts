7db5a83dea602e46c9139735d9128447b328d345
zhoujian <zhoujian.ww@gmail.com>
2023-04-11 13:09:02 +0800

    fix:not export transorformer

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from "./protocol-parser";
 export * from "./binary-protocol.parser";
 export * from "./ascii-protocol.parser";
+export * from "./binary-protocol.transformer";
+export * from "./ascii-protocol.transformer";
