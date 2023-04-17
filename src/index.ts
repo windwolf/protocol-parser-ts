ccd00ee000c4a28917f622ab37f0d0a866e1a134
zhoujian <zhoujian.ww@gmail.com>
2023-04-17 13:24:30 +0800

    split transformer to indepedence package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 export * from "./protocol-parser";
 export * from "./binary-protocol.parser";
 export * from "./ascii-protocol.parser";
-export * from "./binary-protocol.transformer";
-export * from "./ascii-protocol.transformer";
